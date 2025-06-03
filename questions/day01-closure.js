cb56a4b2e971aecb2f6b7556459c817f85147cca
Durai-Prasanth <kdprasanth93@gmail.com>
2025-06-03 08:35:06 +0530

    Add Day 01: JavaScript closure question

---
+++ questions/day01-closure.js
@@ -0,0 +1,14 @@
+// Day 01: What is a closure in JavaScript?
+
+function outer() {
+  let count = 0;
+  return function inner() {
+    count++;
+    console.log(count);
+  };
+}
+
+const counter = outer();
+counter(); // 1
+counter(); // 2
+ 
